b2686870f0049a403d74b2396d6730ead8872015
Davide Carletti <carletti.dav@gmail.com>
2023-10-25 21:15:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+/*let array = [];
+let numero = prompt("inserisci un numero");
+
+array.push(numero);
+numb = parseInt(numero);
+let somma;
+somma = numb;
+console.log(somma);
+
+while (somma < 50) {
+  numero = prompt("inserisci un numero");
+  array.push(numero);
+  numb = parseInt(numero);
+  somma += numb;
+}
+console.log(somma);
+console.log(array);
+*/
+/*
+let numero=prompt('inserisci un numero N')
+for (i=0; numero=i;i++){
+    
+}
+
+*/
+/*
+let invitati = [
+  "marco",
+  "gianna",
+  "enza",
+  "francescone",
+  "giangionni",
+  "roberza",
+];
+//chiedi nome all user
+let nome = prompt("come ti chiami?");
+
+for (i = 0; i <= invitati.length || check === nome; i++) {
+  let check = i;
+  if (check === nome) {
+    alert("bravo");
+  } else {
+    alert("vai via");
+    break;
+  }
+}
+*/
+let mele = [
+  "mela0",
+  "mela1",
+  "mela2",
+  "mela3",
+  "mela4",
+  "mela5",
+  "mela6",
+  "mela7",
+  "mela8",
+  "mela9",
+];
+console.log(mele);
+let mela0 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela1 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela2 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela3 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela4 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela5 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela6 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela7 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela8 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
+let mela9 = {
+  varietà: "1",
+  peso: "1.5",
+  diametro: "3",
+};
